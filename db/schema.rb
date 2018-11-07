788537717382b128a4c631a936884a0c77c2281a
Vijay Tadimeti <tadimetv@oregonstate.edu>
2018-11-06 20:49:42 -0800

    db yaml file change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_07_012845) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
